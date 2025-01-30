57a200c6cc63e46e7108e38065c368a9a021d195
Kristhellfp <159474506+Kristhellfp@users.noreply.github.com>
2025-01-30 16:44:13 -0600

    Add files via upload
    Sistema de carpetas

---
+++ index.js
@@ -0,0 +1,25 @@
+let DOM = document.querySelector("#root");
+ 
+
+let contenedor = document.createElement ('div');
+contenedor.className = "contenedor";
+
+let divheader = document.createElement ('div');
+divheader.className = "div-header"
+contenedor.appendChild (divheader);
+
+let divprogreso = document.createElement ('div');
+divprogreso.className = "div-progreso"
+contenedor.appendChild (divprogreso);
+
+let divtablero = document.createElement ('div');
+divtablero.className = "div-tablero"
+contenedor.appendChild (divtablero);
+
+let divfooter = document.createElement ('div');
+divfooter.className = "div-footer"
+contenedor.appendChild (divfooter);
+
+
+
+DOM.appendChild(contenedor); 
